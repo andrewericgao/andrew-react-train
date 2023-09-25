1ac6935d907b3ad0c88db3e73362d68eb07d643d
Andrewegao <andrewgao99@gmail.com>
2023-09-25 10:41:52 -0500

    fixing firebase

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDz8mJFpFpdA2hJoO1z3MizYOhygsc038U",
+  authDomain: "andrewreacttrain.firebaseapp.com",
+  databaseURL: "https://andrewreacttrain-default-rtdb.firebaseio.com",
+  projectId: "andrewreacttrain",
+  storageBucket: "andrewreacttrain.appspot.com",
+  messagingSenderId: "783373355244",
+  appId: "1:783373355244:web:9fea2676dd5f9e49aa7dc9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
